041cf1cce7b50f6e3459428d966691b712672ea0
Vanah333 <sitehars@gmail.com>
2026-05-14 15:03:28 +0300

    feat: add prisma config and swagger setup

--- backend-she-devs/src/main.ts
+++ backend-she-devs/src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  // Configuration Swagger
+  const config = new DocumentBuilder()
+    .setTitle('She-Devs API')
+    .setDescription('Documentation de l\'API She-Devs')
+    .setVersion('1.0')
+    .addBearerAuth() // si tu utilises JWT
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document); // accessible sur /api
+
+  await app.listen(3000);
 }
 bootstrap();
