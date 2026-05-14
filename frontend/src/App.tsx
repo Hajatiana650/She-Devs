162c1f6587a85a69ea50c456ba97b09d65ac66d6
Fanasina Binah <fanasinabinah@gmail.com>
2026-05-14 14:49:10 +0300

    create login page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Dashboard from "./pages/dashboard";
-
+import Login from "./pages/login";
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/login" element={<Login />} />
+
       </Routes>
     </BrowserRouter>
   );
