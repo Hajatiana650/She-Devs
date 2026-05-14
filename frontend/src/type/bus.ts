494f7c77cdd98bd94afcc1f88da1b68c48785ec3
Fanasina Binah <fanasinabinah@gmail.com>
2026-05-15 00:56:15 +0300

    Integrate Dechets

--- frontend/src/type/bus.ts
+++ frontend/src/type/bus.ts
@@ -4,6 +4,10 @@ export interface Bus {
   bus_status: boolean;
   id_line: number;
   id_localisation: number;
+  createdAt?: string;
+  updatedAt?: string;
+
+  last_seen_at?: string;
   line: {
     id_line: number;
     nb_line: string;
@@ -24,8 +28,35 @@ export interface Bus {
   };
 }
 
+
+
 export interface BusResponse {
   statusCode: number;
   message: string;
   data: Bus[];
 }
+
+export interface BusLine {
+  id_line: number;
+  nb_line: string;
+}
+
+export interface BusLocalisation {
+  id_localisation: number;
+  longitude: number;
+  latitude: number;
+}
+
+export interface User {
+  id_user: number;
+  user_name: string;
+  email: string;
+}
+
+export interface Driver {
+  id_driver: number;
+  id_user: number;
+  id_bus: number;
+  user: User;
+}
+
---
+++ frontend/src/type/visit.ts
@@ -0,0 +1,19 @@
+export interface Visit {
+  id: number;
+  busId: number;
+  visitType: "TECHNICAL_VISIT" | "CONTROL" | "OTHER";
+  result: "APTE" | "INAPTE";
+  dateVisit: string;
+  dateLimit: string;
+  observation?: string;
+  attachment?: string;
+
+  createdAt: string;
+  updatedAt: string;
+}
+
+export interface VisitResponse {
+  statusCode: number;
+  message: string;
+  data: Visit;
+}
