105d8acd32b93bba36412fbc660ca38070dc13a5
Fanasina Binah <fanasinabinah@gmail.com>
2026-05-14 16:10:04 +0300

    create chauffeur profil

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Dashboard from "./pages/dashboard";
 import Login from "./pages/login";
+
 function App() {
   return (
     <BrowserRouter>
